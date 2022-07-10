1952ead6b1bd7dfb4f7a72376e3a430b580172ac
yasszxd <abdelhedi166@gmail.com>
2022-07-10 20:10:59 +0100

    Quran APP

--- app/src/main/java/com/example/quranv01/PageAdapter.java
+++ app/src/main/java/com/example/quranv01/PageAdapter.java
@@ -1,6 +1,15 @@
 package com.example.quranv01;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
+import android.text.Spannable;
+import android.text.SpannableString;
+import android.text.SpannableStringBuilder;
+import android.text.Spanned;
+import android.text.style.AbsoluteSizeSpan;
+import android.text.style.BackgroundColorSpan;
+import android.text.style.ForegroundColorSpan;
+import android.text.style.StyleSpan;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -25,9 +34,11 @@ public class PageAdapter extends RecyclerView.Adapter<PageAdapter.ViewHolder> {
     public class ViewHolder extends RecyclerView.ViewHolder{ // 3melna View holder mte3na
 
         TextView page;
+        TextView pageNumber;
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
             page = itemView.findViewById(R.id.page);
+            pageNumber = itemView.findViewById(R.id.pageNumber);
             itemView.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
@@ -44,12 +55,22 @@ public class PageAdapter extends RecyclerView.Adapter<PageAdapter.ViewHolder> {
         return new ViewHolder(v);
     }
 
+    @SuppressLint("ResourceAsColor")
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
         Pagetext = "";
-            for (j = 0 ; j<pages.get(position).ayas.size();j++)
-                Pagetext+=pages.get(position).ayas.get(j).text + (pages.get(position).ayas.get(j).ayaNumber+1);
-        holder.page.setText(Pagetext);
+        int start;
+        SpannableStringBuilder spannableString = new SpannableStringBuilder();
+            for (j = 0 ; j<pages.get(position).ayas.size();j++) {
+                spannableString.append(pages.get(position).ayas.get(j).text );
+                start = spannableString.length();
+                spannableString.append("  "+ (pages.get(position).ayas.get(j).ayaNumber + 1) +"  ");
+                spannableString.setSpan(new BackgroundColorSpan(R.color.black), start+1, spannableString.length()-1, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+
+                // Pagetext += pages.get(position).ayas.get(j).text + " " + (pages.get(position).ayas.get(j).ayaNumber + 1) + " ";
+            }
+        holder.page.setText(spannableString);
+        holder.pageNumber.setText(Integer.toString(pages.get(position).getPageNumber()));
     }
 
     @Override
